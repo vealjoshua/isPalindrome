64d1110a47a87b50e7b47c0a4b47582f1fc6a51e
Joshua Veal-Briscoe <jkv2c9@mail.umsl.edu>
2017-11-23 08:44:43 -0600

    Better checking.

--- isPalindrome.c
+++ isPalindrome.c
@@ -1,13 +1,14 @@
 bool checkPalindrome(char * inputString) {
-    bool isPalindrome = false;
+    bool isPalindrome = true;
     size_t length = strlen(inputString);
     for (int i = 0; i < ((length % 2) == 0 ? length/2: length/2 + 1); i++)
     {
         printf("%c, %c\n", inputString[i], inputString[length - i - 1]);
-        if (inputString[i] == inputString[length - i - 1])
-            isPalindrome = true;
-        else
+        if (inputString[i] != inputString[length - i - 1])
+        {
             isPalindrome = false;
+            return isPalindrome;
+        }
     }
     return isPalindrome;
 }
